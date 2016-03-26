5edaff39a2275ffd034c8de7fc8126f9a8dea600
Evan Herman <evan@yikesinc.com>
2016-03-26 10:56:28 -0400

    update tests

--- tests/test-timeline-express-base.php
+++ tests/test-timeline-express-base.php
@@ -9,15 +9,17 @@
 class TE_Base_Tests extends WP_UnitTestCase {
 
 	// store global options
-	public $options;
+	public $options, $base_class;
 
 	public function setUp() {
 		$this->options = timeline_express_get_options();
+		include_once( TIMELINE_EXPRESS_PATH . 'lib/classes/class.timeline-express.php' );
+		$this->$base_class = new TimelineExpressBase();
 	}
 
 	// test plugin activation
 	public function test_timeline_express_activate() {
-		timeline_express_activate();
+		$this->$base_class->timeline_express_activate();
 		$this->assertEquals( get_option( 'timeline_express_do_activation_redirect' ), true );
 	}
 }
