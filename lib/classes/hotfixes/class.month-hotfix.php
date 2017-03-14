7ef652555b2066b7cbdaf8b1adc2d43fa18f5e0c
Evan Herman <evan.m.herman@gmail.com>
2017-03-14 09:28:42 -0400

    Update i10n months with new strings

--- lib/classes/hotfixes/class.month-hotfix.php
+++ lib/classes/hotfixes/class.month-hotfix.php
@@ -17,6 +17,8 @@ class Timeline_Express_Month_Hotfix {
 
 	public function __construct( $date ) {
 
+		wp_die( get_locale() );
+
 		$this->date        = $date;
 		$this->en_months   = array( 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' );
 		$this->i10n_months = $this->get_i10n_months();
@@ -61,6 +63,17 @@ class Timeline_Express_Month_Hotfix {
 		$i10n_months = array();
 
 		$i10n_months['de_DE'] = array( 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember' );
+		$i10n_months['fr_FR'] = array( 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' );
+		$i10n_months['it_IT'] = array( 'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre' );
+		$i10n_months['da_DK'] = array( 'Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December' );
+		$i10n_months['es_ES'] = array( 'Enero ', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' );
+		$i10n_months['ja'] = array( '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' );
+		$i10n_months['nl_NL'] = array( 'Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December' );
+		$i10n_months['pl_PL'] = array( 'Stycznia', 'Lutego', 'Marca', 'Kwietnia', 'Maja', 'Czerwca', 'Lipca', 'Sierpnia', 'Września', 'Października', 'Listopada', 'Grudnia' );
+		$i10n_months['pt_BR'] = array( 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' );
+		$i10n_months['pt_PT'] = array( 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' );
+		$i10n_months['tr_TR'] = array( 'Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık' );
+		$i10n_months['zh_TW'] = array( '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月' );
 
 		return apply_filters( 'timeline_express_i10n_months', $i10n_months );
 
