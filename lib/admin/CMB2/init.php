c7e9b0f756fdae53db898cbdf1a0433faf94dcf0
fliespl <a.rzadkowolski@gmail.com>
2017-04-23 12:10:33 +0200

    Patch CMB2 - conflicts with other autoload scripts
    https://github.com/CMB2/CMB2/issues/886
    https://github.com/CMB2/CMB2/commit/5428430312d54390fe2370b316d2916362d35f8e

--- lib/admin/CMB2/init.php
+++ lib/admin/CMB2/init.php
@@ -150,7 +150,7 @@ if ( ! class_exists( 'CMB2_Bootstrap_223_Trunk', false ) ) {
 			spl_autoload_register( 'cmb2_autoload_classes' );
 
 			// Kick the whole thing off
-			require_once 'bootstrap.php';
+			require_once( cmb2_dir( 'bootstrap.php' ) );
 			cmb2_bootstrap();
 		}
 
